2a2931d3feb170229d6ba39d375b5ff267fb8ac2
ErinKace <erinkacerovskis@gmail.com>
2023-05-17 20:12:45 -0500

    finished everything but bonus mission

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface candidate {
+  name:string;
+  photo: string;
+}
+
 @Component({
   selector: 'app-crew',
   templateUrl: './crew.component.html',
@@ -8,9 +13,10 @@ import { Component, OnInit } from '@angular/core';
 export class CrewComponent implements OnInit {
 
   inCrew: boolean = false;
-  crew: object[] = [];
+  crew: candidate[] = [];
+  selectedCandidate: candidate;
 
-  candidates: object[] = [
+  candidates: candidate[] = [
     {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
     {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
     {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
@@ -25,5 +31,24 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
+  addCrewMember(crewPerson:candidate) {
+    this.inCrew = this.crew.includes(crewPerson);
+    if (this.inCrew) {
+      let personIndex = this.crew.indexOf(crewPerson);
+      this.crew.splice(personIndex,1);
+    } else if (!this.inCrew && this.crew.length < 3) {
+      this.crew.push(crewPerson);
+    }
+  }
+  selected(crewPerson:candidate) {
+    this.selectedCandidate = crewPerson
+  }
+
+  doesInclude(crewPerson:candidate):boolean {
+    if (this.crew.includes(crewPerson)) {
+      return true;
+    }
+    return false;
+  }
 
 }
